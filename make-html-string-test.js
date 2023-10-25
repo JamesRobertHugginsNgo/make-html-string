2bc4541b4f082418a5ecc022a04a3b258f2f5190
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2023-10-25 00:22:21 -0400

    Add void elements, self closing options and docs

--- make-html-string-test.js
+++ make-html-string-test.js
@@ -1,19 +1,129 @@
-import { test, testGroup} from 'test-group';
+/* global console */
 
-import makeHtmlString from './make-html-string.js';
+import { test, testGroup } from 'test-group';
 
-testGroup('Using empty argument', () => {
-	const htmlString = makeHtmlString();
+import makeHtmlString, { makeXmlString } from './make-html-string.js';
 
-	test('Result is empty div element', () => {
-		return htmlString === '<div></div>';
+testGroup('Make HTML String'.toUpperCase(), () => {
+	testGroup('With No Definition', () => {
+		const htmlString = makeHtmlString();
+		test('Result Should Be an Empty DIV Tag', () => {
+			console.log(htmlString);
+			return htmlString === '<div></div>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\' }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span' });
+		test('Result Should Be an Empty SPAN Tag', () => {
+			console.log(htmlString);
+			return htmlString === '<span></span>';
+		});
+	});
+
+	testGroup('With "{ name: \'br\' }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'br' });
+		test('Result Should Be a Void BR Tag', () => {
+			console.log(htmlString);
+			return htmlString === '<br>';
+		});
+	});
+
+	testGroup('With "{ name: \'br\', voidElements: null }" Definition Ignoring Void Elements', () => {
+		const htmlString = makeHtmlString({ name: 'br', voidElements: null });
+		test('Result Should Be an Empty BR Tag', () => {
+			console.log(htmlString);
+			return htmlString === '<br></br>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', attributes: { \'class\': \'class-name\' } }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', attributes: { 'class': 'class-name' } });
+		test('Result Should Be an Empty SPAN Tag With a Class Attribute', () => {
+			console.log(htmlString);
+			return htmlString === '<span class="class-name"></span>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', children: true }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', children: true });
+		test('Result Should Be a SPAN Tag With "true" Text', () => {
+			console.log(htmlString);
+			return htmlString === '<span>true</span>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', children: 123 }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', children: 123 });
+		test('Result Should Be a SPAN Tag With "123" Text', () => {
+			console.log(htmlString);
+			return htmlString === '<span>123</span>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', children: \'Hello World\' }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', children: 'Hello World' });
+		test('Result Should Be a SPAN Tag With "Hello World" Text', () => {
+			console.log(htmlString);
+			return htmlString === '<span>Hello World</span>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', children: { name: \'strong\', children: \'Hello World\' } }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', children: { name: 'strong', children: 'Hello World' } });
+		test('Result Should Be a SPAN Tag With a STRONG tag With "Hello World" Text', () => {
+			console.log(htmlString);
+			return htmlString === '<span><strong>Hello</strong> World</span>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', children: [\'Hello\', \' \', \'World\'] }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', children: ['Hello', ' ', 'World'] });
+		test('Result Should Be a SPAN Tag With "Hello World" Text', () => {
+			console.log(htmlString);
+			return htmlString === '<span><strong>Hello World</strong></span>';
+		});
+	});
+
+	testGroup('With "{ name: \'span\', children: [{ name: \'strong\', children: \'Hello\' }, \' \', \'World\'] }" Definition', () => {
+		const htmlString = makeHtmlString({ name: 'span', children: [{ name: 'strong', children: 'Hello' }, ' ', 'World'] });
+		test('Result Should Be a SPAN Tag With "Hello World" Text where "Hello" Is Inside a STRONG Tag', () => {
+			console.log(htmlString);
+			return htmlString === '<span><strong>Hello</strong> World</span>';
+		});
 	});
 });
 
-testGroup('Using name only', () => {
-	const htmlString = makeHtmlString({ name: 'br' });
+testGroup('Make XML String'.toUpperCase(), () => {
+	testGroup('With No Definition', () => {
+		const xmlString = makeXmlString();
+		test('Result Should Be a Self Closing DIV Tag', () => {
+			console.log(xmlString);
+			return xmlString === '<div />';
+		});
+	});
 
-	test('Result is empty br element', () => {
-		return htmlString === '<br>';
+	testGroup('With "{ name: \'svg\' }" Definition', () => {
+		const xmlString = makeXmlString({ name: 'svg' });
+		test('Result Should Be a Self Closing SVG Tag', () => {
+			console.log(xmlString);
+			return xmlString === '<svg />';
+		});
+	});
+
+	testGroup('With "{ name: \'br\' }" Definition', () => {
+		const xmlString = makeXmlString({ name: 'br' });
+		test('Result Should Be a Self Closing BR Tag', () => {
+			console.log(xmlString);
+			return xmlString === '<br />';
+		});
+	});
+
+	testGroup('With "{ name: \'br\', selfClosing: false }" Definition', () => {
+		const xmlString = makeXmlString({ name: 'br', selfClosing: false });
+		test('Result Should Be an Empty BR Tag', () => {
+			console.log(xmlString);
+			return xmlString === '<br></br>';
+		});
 	});
 });
--- make-html-string.js
+++ make-html-string.js
@@ -1,35 +1,84 @@
-export default function makeHtmlString(definition = {}) {
-	if (Array.isArray(definition)) {
-		const elementArray = [];
+/**
+ * A array of HTML names for void elements.
+ * @type {[string]}
+ */
+export const htmlVoidElements = [
+	'area',
+	'base',
+	'br',
+	'col',
+	'embed',
+	'hr',
+	'img',
+	'input',
+	'link',
+	'meta',
+	'param',
+	'source',
+	'track',
+	'wbr'
+];
 
-		const length = definition.length;
-		for (let index = 0; index < length; index++) {
-			const value = makeHtmlString(definition[index]);
-			if (value == null) continue;
-
-			elementArray.push(value);
-		}
-
-		return elementArray.join('');
+/**
+ * Returns a valid HTML tag as a string.
+ * @param {any} [definition={}]
+ * @param {object} [options={}]
+ * @param {object} [options.selfClosing=false]
+ * @param {[string]} [options.voidElements=htmlVoidElements]
+ * @returns {string}
+ */
+export default function makeHtmlString(definition = {}, options = {}) {
+	if (definition == null) {
+		return '';
 	}
 
-	if (definition && typeof definition === 'object') {
+	if (typeof definition === 'object') {
+		if (Array.isArray(definition)) {
+			const {
+				selfClosing = false,
+				voidElements = htmlVoidElements
+			} = options;
+
+			const elementArray = [];
+
+			const length = definition.length;
+			for (let index = 0; index < length; index++) {
+				const value = definition[index];
+				if (value == null) continue;
+
+				elementArray.push(makeHtmlString(value, { selfClosing, voidElements }));
+			}
+
+			return elementArray.join('');
+		}
+
+		let {
+			selfClosing = false,
+			voidElements = htmlVoidElements
+		} = options;
+
 		const {
 			name = 'div',
 			attributes,
-			children
+			children,
+			selfClosing: selfClosingDefinition,
+			voidElements: voidElementsDefinition
 		} = definition;
+
+		if (selfClosingDefinition !== undefined) {
+			selfClosing = selfClosingDefinition;
+		}
+		if (voidElementsDefinition !== undefined) {
+			voidElements = voidElementsDefinition;
+		}
 
 		const elementArray = ['<'];
 
 		const tagArray = [name];
-
 		if (attributes) {
 			for (const key in attributes) {
 				const value = attributes[key];
-				if (value == null) {
-					continue;
-				}
+				if (value == null) continue;
 
 				if (value === '') {
 					tagArray.push(key);
@@ -39,21 +88,44 @@ export default function makeHtmlString(definition = {}) {
 				tagArray.push(`${key}="${value}"`);
 			}
 		}
-
 		elementArray.push(tagArray.join(' '));
 
-		elementArray.push('>');
-
-		if (children != null) {
-			elementArray.push(makeHtmlString(children));
-		}
-
-		if (children || name === 'div') {
-			elementArray.push('</', name, '>');
+		if (!voidElements || !voidElements.includes(name)) {
+			if (children == null) {
+				if (selfClosing) {
+					elementArray.push(' />');
+				} else {
+					elementArray.push('></', name, '>');
+				}
+			} else {
+				elementArray.push(
+					'>',
+					makeHtmlString(children, { selfClosing, voidElements }),
+					'</', name, '>'
+				);
+			}
+		} else {
+			elementArray.push('>');
 		}
 
 		return elementArray.join('');
 	}
 
 	return String(definition);
+}
+
+/**
+ * Returns a valid XML tag as a string.
+ * @param {any} [definition={}]
+ * @param {object} [options={}]
+ * @param {object} [options.selfClosing=true]
+ * @param {[string]} [options.voidElements=null]
+ * @returns {string}
+ */
+export function makeXmlString(definition = {}, options = {}) {
+	const {
+		selfClosing = true,
+		voidElements = null
+	} = options;
+	return makeHtmlString(definition, { selfClosing, voidElements });
 }
