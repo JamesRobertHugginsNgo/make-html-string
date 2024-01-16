1b5750c40d43c14e5ef1cc636d4627a15941f3c9
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-01-16 00:14:29 -0500

    Simplify Usage

--- make-html-string.js
+++ make-html-string.js
@@ -1,92 +1,87 @@
-/**
- * A array of HTML names for void elements.
- * @type {[string]}
- */
-export const HTML_VOID_ELEMENTS
-	= ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
+// ==
+// CONSTANT: HTML VOID ELEMENTS
+// ==
 
-/**
- * Returns a valid HTML tag as a string.
- * @param {any} [definition={}]
- * @param {object} [options={}]
- * @param {object} [options.selfClosing=false]
- * @param {[string]} [options.voidElements=htmlVoidElements]
- * @returns {string}
- */
-export default function makeHtmlString(definition = {}, options = {}) {
-	if (definition == null) {
-		return '';
+export const HTML_VOID_ELEMENTS = [
+	'area',
+	'base',
+	'br',
+	'col',
+	'embed',
+	'hr',
+	'img',
+	'input',
+	'link',
+	'meta',
+	'param',
+	'source',
+	'track',
+	'wbr'
+];
+
+// ==
+// FUNCTION: MAKE HTML STRING
+// ==
+
+export default function makeHtmlString(
+	{
+		name = 'div',
+		attributes,
+		children,
+		selfClosing = false,
+		voidElements = HTML_VOID_ELEMENTS
+	} = {}
+) {
+	const openTag = [name];
+	if (attributes) {
+		for (const key in attributes) {
+			const value = attributes[key];
+			if (value != null) {
+				if (value === '') {
+					openTag.push(key);
+				} else {
+					openTag.push(`${key}="${value}"`);
+				}
+			}
+		}
 	}
 
-	if (typeof definition === 'object') {
-		const {
-			selfClosing = false,
-			voidElements = HTML_VOID_ELEMENTS
-		} = options;
-
-		if (Array.isArray(definition)) {
-			const elementArray = [];
-
-			const length = definition.length;
+	const htmlArray = ['<', openTag.join(' ')];
+	if (voidElements.includes(name)) {
+		if (selfClosing) {
+			htmlArray.push(' />');
+		} else {
+			htmlArray.push('>');
+		}
+	} else {
+		const childrenArray = [];
+		if (children) {
+			const length = children.length;
 			for (let index = 0; index < length; index++) {
-				const value = definition[index];
-				if (value == null) continue;
-
-				elementArray.push(makeHtmlString(value, { selfClosing, voidElements }));
-			}
-
-			return elementArray.join('');
-		}
-
-		const {
-			name = 'div',
-			attributes,
-			children,
-			selfClosing: selfClosingDefinition = selfClosing,
-			voidElements: voidElementsDefinition = voidElements
-		} = definition;
-
-		const elementArray = ['<'];
-
-		const tagArray = [name];
-		if (attributes) {
-			for (const key in attributes) {
-				const value = attributes[key];
-				if (value == null) continue;
-
-				if (value === '') {
-					tagArray.push(key);
-					continue;
+				const child = children[index];
+				if (child != null) {
+					if (typeof child !== 'object') {
+						childrenArray.push(child);
+					} else {
+						childrenArray.push(makeHtmlString({
+							selfClosing,
+							voidElements,
+							...child
+						}));
+					}
 				}
-
-				tagArray.push(`${key}="${value}"`);
 			}
 		}
-		elementArray.push(tagArray.join(' '));
-
-		if (!voidElementsDefinition || !voidElementsDefinition.includes(name)) {
-			if (children == null) {
-				if (selfClosingDefinition) {
-					elementArray.push(' />');
-				} else {
-					elementArray.push('></', name, '>');
-				}
+		if (childrenArray.length === 0) {
+			if (selfClosing) {
+				htmlArray.push(' />');
 			} else {
-				elementArray.push(
-					'>',
-					makeHtmlString(children, {
-						selfClosing: selfClosingDefinition,
-						voidElements: voidElementsDefinition
-					}),
-					'</', name, '>'
-				);
+				htmlArray.push('></', name, '>');
 			}
 		} else {
-			elementArray.push('>');
+			htmlArray.push('>', ...childrenArray, '</', name, '>');
 		}
-
-		return elementArray.join('');
 	}
 
-	return String(definition);
+	return htmlArray.join('');
 }
